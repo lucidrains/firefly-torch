7069de73d2550d01535ea9ca897895908ae2a4c8
lucidrains <lucidrains@gmail.com>
2024-09-08 09:14:41 -0700

    better behaving fireflies

--- firefly.py
+++ firefly.py
@@ -1,4 +1,5 @@
 import torch
+import einx
 
 # test objective function - solution is close to all 1.'s
 
@@ -8,15 +9,15 @@ def rosenbrock(x):
 # constants
 
 steps = 1000
-population_size = 100
+population_size = 200
 dimensions = 4
 lower_bound = -4.
 upper_bound = 4.
 
 beta0 = 2.           # exploitation factor, moving fireflies of low light intensity to high
 gamma = 1.           # controls light intensity decay over distance
-alpha = 0.2          # exploration factor
-alpha_decay = 0.99   # exploration decay each step
+alpha = 0.1          # exploration factor
+alpha_decay = 0.8    # exploration decay each step
 
 # main algorithm
 
@@ -32,24 +33,29 @@ for _ in range(steps):
 
     # fireflies with lower light intensity (high cost) moves towards the higher intensity (lower cost)
 
-    move_mask = costs[:, None] > costs[None, :]
+    move_mask = einx.greater('i, j -> i j', costs, costs)
 
     # get vectors of fireflies to one another
     # calculate distance and the beta
 
-    delta_positions = fireflies[None, :] - fireflies[:, None]
+    delta_positions = einx.subtract('j d, i d -> i j d', fireflies, fireflies)
 
     distance = delta_positions.norm(dim = -1)
 
     betas = beta0 * (-gamma * distance ** 2).exp()
 
+    # calculate movements
+
+    attraction = einx.multiply('i j, i j d -> i j d', move_mask * betas, delta_positions)
     random_walk = alpha * (torch.rand_like(fireflies) - 0.5) * (upper_bound - lower_bound)
 
     # move the fireflies
 
-    fireflies += ((move_mask * betas)[..., None] * delta_positions).sum(dim = -2)
+    fireflies += attraction.sum(dim = 1)
 
     fireflies += random_walk
+
+    fireflies.clamp_(min = lower_bound, max = upper_bound)
 
     # decay exploration factor
 
